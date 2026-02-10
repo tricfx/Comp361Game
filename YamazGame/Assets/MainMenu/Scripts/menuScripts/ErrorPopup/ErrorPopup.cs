94e2a8935d7d6afce4048fc413bcd4018992343f
rasewill <nizarmaazouz@gmail.com>
2026-02-10 02:49:49 -0500

    added login/register/forgotpw error popups

---
+++ YamazGame/Assets/MainMenu/Scripts/menuScripts/ErrorPopup/ErrorPopup.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+
+public class ErrorPopup : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup[] screens;
+    [SerializeField] private GameObject[] backgrounds;
+
+    [SerializeField] private TMP_Text errorMsg;
+
+    public void Show(string msg)
+    {
+        errorMsg.text = msg;
+
+        for (int i = 0; i < backgrounds.Length; i++)
+            if (backgrounds[i] != null) backgrounds[i].SetActive(false);
+
+        int bestIdx = 0;
+        float bestAlpha = -1f;
+
+        for (int i = 0; i < screens.Length; i++)
+        {
+            var cg = screens[i];
+            if (cg == null) continue;
+            if (!cg.gameObject.activeInHierarchy) continue;
+
+            if (cg.alpha > bestAlpha)
+            {
+                bestAlpha = cg.alpha;
+                bestIdx = i;
+            }
+        }
+
+        if (bestIdx >= 0 && bestIdx < backgrounds.Length && backgrounds[bestIdx] != null)
+            backgrounds[bestIdx].SetActive(true);
+
+        gameObject.SetActive(true);
+        transform.SetAsLastSibling();
+    }
+
+    public void Hide() => gameObject.SetActive(false);
+}
