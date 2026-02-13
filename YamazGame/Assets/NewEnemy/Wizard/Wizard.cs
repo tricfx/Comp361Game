f0cae27b65b288b641a1bba410c411e09cf90144
jmmyzhang <155680424+jmmyzhang@users.noreply.github.com>
2026-02-12 20:29:29 -0500

    init wizard enemy

---
+++ YamazGame/Assets/NewEnemy/Wizard/Wizard.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Wizard : NewEnemy
+{
+    public override void Attack()
+    {
+        
+    }
+
+    public override void ResetAttack()
+    {
+        
+    }
+
+    public override void Move(Vector2 startPosition, Vector2 targetPosition)
+    {
+        
+    }
+}
