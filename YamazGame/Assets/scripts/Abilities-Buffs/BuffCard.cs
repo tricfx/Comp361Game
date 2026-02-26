1fcaa7e3bcacff14de93050f2ba853e611678d03
Isaiah Abella <isaiah.abella@mail.mcgill.ca>
2026-02-25 23:41:24 -0500

    Card UI update

--- YamazGame/Assets/scripts/Abilities-Buffs/BuffCard.cs
+++ YamazGame/Assets/scripts/Abilities-Buffs/BuffCard.cs
@@ -5,11 +5,13 @@ public class BuffCard : Card
 {
     public string buffID;
     public int bonusDamage;
+    public int bonusHealth;
 
     public override void Apply(Player player)
     {
         //this is an example idk what we want yet
         player.attackDamage += bonusDamage;
+        player.maxHP += bonusHealth;
         player.AddBuff(buffID);
     }
 }
