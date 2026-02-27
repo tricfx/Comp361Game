95d19fb7ad1e56a281a5dfc612828b0b51d162e5
Ayman <aymanhayari@gmail.com>
2026-02-27 13:14:00 -0500

    fixed menu

--- YamazGame/Assets/scripts/pauseMenu.cs
+++ YamazGame/Assets/scripts/pauseMenu.cs
@@ -33,9 +33,11 @@ public class pauseMenu : MonoBehaviour
             // If in Controls → go back to Settings
             if (inControls)
             {
+                Debug.Log("Leaving Controls, going back to Settings");
                 Controls.gameObject.SetActive(false);
                 Settings.gameObject.SetActive(true);
                 inControls = false;
+                inSettings = true;
                 return;
             }
 
@@ -58,13 +60,23 @@ public class pauseMenu : MonoBehaviour
         Settings.gameObject.SetActive(true);
         PauseMenu.gameObject.SetActive(false);
         inSettings = true;
+        inControls = false;
     }
 
+    public void pauseMenuScreen()
+    {
+        Settings.gameObject.SetActive(false);
+        PauseMenu.gameObject.SetActive(true);
+        inSettings = false;
+        inControls = false;
+        isPaused = true;
+    }
     public void ControlsMenu()
     {
         Controls.gameObject.SetActive(true);
         Settings.gameObject.SetActive(false);
         inControls = true;
+        inSettings = false;
     }
 
     public void Resume()
@@ -89,9 +101,11 @@ public class pauseMenu : MonoBehaviour
     public void LeavingSettings()
         {
         inSettings = false;
+        inControls = true;
     }
     public void LeavingControls()
     {
         inControls = false;
+        inSettings = true;
     }
 }
