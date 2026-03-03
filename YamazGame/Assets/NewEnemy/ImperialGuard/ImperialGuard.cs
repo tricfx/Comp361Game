5cf7a57f9be26725cb14eab094bf3cc92c2e03cd
jmmyzhang <jimmy.zhang92@gmail.com>
2026-03-03 13:07:25 -0500

    new enemy anims

---
+++ YamazGame/Assets/NewEnemy/ImperialGuard/ImperialGuard.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ImperialGuard : NewEnemy
+{
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Move(Vector2 startPosition, Vector2 targetPosition)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ResetAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
