6cfb48c31fc5ac1ff75598f9a074a1bab6be96c8
Eiji Higa <eijisht@gmail.com>
2026-03-04 18:08:24 -0500

    fixed collisions, scene change, added camera bounds, rug and door sprites

---
+++ YamazGame/Assets/scripts/Map/EnemyManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System;
+using System.Diagnostics;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static event Action OnAllEnemiesDefeated;
+
+    private int aliveEnemies = 0;
+
+    private void Start()
+    {
+        UnityEngine.Debug.Log($"Total enemies {aliveEnemies}");
+        if (aliveEnemies == 0)
+        {
+            UnityEngine.Debug.LogWarning("No enemies registered in scene.");
+        }
+    }
+
+    public void RegisterEnemy()
+    {
+        aliveEnemies++;
+    }
+
+    public void UnregisterEnemy()
+    {
+        aliveEnemies--;
+
+        if (aliveEnemies <= 0)
+        {
+            UnityEngine.Debug.Log("All enemies defeated");
+            OnAllEnemiesDefeated?.Invoke();
+        }
+    }
+}
