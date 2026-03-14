84da700f5df62e0b4ed994e08125890da1822ebc
jmmyzhang <jimmy.zhang92@gmail.com>
2026-03-14 18:04:18 -0400

    implement sorcerer

--- YamazGame/Assets/NewEnemy/Sorcerer/Sorcerer.cs
+++ YamazGame/Assets/NewEnemy/Sorcerer/Sorcerer.cs
@@ -1,19 +1,61 @@
+using System.Collections;
 using UnityEngine;
 
 public class Sorcerer : NewEnemy
 {
+    [SerializeField] SorcererSpellSpawner spellSpawner;
+    [SerializeField] float spellTimer = 1f;
+
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(SpellRoutine());
+    }
+
+    IEnumerator SpellRoutine()
+    {
+        while (true)
+        {
+            if (!Targetable)
+            {
+                yield break;
+            }
+
+            if (detectionRange.PlayerInRange)
+            {
+                spellSpawner.SpawnSpell();
+                yield return new WaitForSeconds(spellTimer);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
     public override void Attack()
     {
-        throw new System.NotImplementedException();
+        if (!CanAttack) return;
+
+        CanAttack = false;
+        animator.SetTrigger("attack");
+        Invoke(nameof(ResetAttack), _attackCooldown);
     }
 
     public override void Move(Vector2 startPosition, Vector2 targetPosition)
     {
-        throw new System.NotImplementedException();
+        Vector2 seek = (targetPosition - startPosition).normalized;
+        Vector2 separation = GetSeparationForce();
+        Vector2 avoid = GetObstacleAvoidance(seek);
+        Vector2 noise = Random.insideUnitCircle * 0.1f;
+
+        Vector2 finalDir = (seek + separation + avoid + noise).normalized;
+        rb.AddForce(finalDir * _moveSpeed * Time.fixedDeltaTime);
+        FacePlayer(startPosition, targetPosition);
     }
 
     public override void ResetAttack()
     {
-        throw new System.NotImplementedException();
+        CanAttack = true;
     }
 }
--- YamazGame/Assets/NewEnemy/Sorcerer/SorcererSpell/SorcererSpell.cs
+++ YamazGame/Assets/NewEnemy/Sorcerer/SorcererSpell/SorcererSpell.cs
@@ -1,16 +1,38 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class SorcererSpell : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Collider2D hurtbox;
+    bool hasHit = false;
+
     void Start()
     {
-        
+        hurtbox = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void DestroySpell()
     {
-        
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasHit) return;
+        if (!other.CompareTag("PlayerHitbox")) return;
+
+        hasHit = true;
+        hurtbox.enabled = false;
+    }
+
+    void EnableHurtbox()
+    {
+        if (hasHit) return;
+        hurtbox.enabled = true;
+    }
+
+    void DisableHurtbox()
+    {
+        hurtbox.enabled = false;
     }
 }
