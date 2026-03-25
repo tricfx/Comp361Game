7bfe9bb832b87c9920df1a375b55c417bf3475e0
jmmyzhang <155680424+jmmyzhang@users.noreply.github.com>
2026-03-25 15:30:20 -0400

    finish deathbringer, need to fix bugs tho

--- YamazGame/Assets/NewEnemy/Deathbringer/Deathbringer.cs
+++ YamazGame/Assets/NewEnemy/Deathbringer/Deathbringer.cs
@@ -3,15 +3,32 @@ using UnityEngine;
 public class Deathbringer : NewEnemy
 {
     [SerializeField] DeathbringerSpellSpawner spellSpawner;
+    [SerializeField] EnemyAttackRange meleeRange;
 
     public override void Attack()
     {
-        
+        if (!CanAttack) return;
+
+        CanAttack = false;
+        animator.SetTrigger(meleeRange.PlayerInRange ? "attack" : "spell");
+        Invoke(nameof(ResetAttack), _attackCooldown);
+    }
+
+    public void Spell()
+    {
+        spellSpawner.SpawnSpell();
     }
 
     public override void Move(Vector2 startPosition, Vector2 targetPosition)
     {
-        
+        Vector2 seek = (targetPosition - startPosition).normalized;
+        Vector2 separation = GetSeparationForce();
+        Vector2 avoid = GetObstacleAvoidance(seek);
+        Vector2 noise = Random.insideUnitCircle * 0.1f;
+
+        Vector2 finalDir = (seek + separation + avoid + noise).normalized;
+        rb.AddForce(finalDir * _moveSpeed * Time.fixedDeltaTime);
+        FacePlayer(startPosition, targetPosition);
     }
 
     public override void ResetAttack()
