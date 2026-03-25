670e0a9a17a2e77d09c401b6e24195e7d8c53d00
jmmyzhang <155680424+jmmyzhang@users.noreply.github.com>
2026-03-24 20:13:30 -0400

    deathbringer progress

--- YamazGame/Assets/NewEnemy/Deathbringer/Deathbringer.cs
+++ YamazGame/Assets/NewEnemy/Deathbringer/Deathbringer.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class Deathbringer : NewEnemy
 {
+    [SerializeField] DeathbringerSpellSpawner spellSpawner;
+
     public override void Attack()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void Move(Vector2 startPosition, Vector2 targetPosition)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void ResetAttack()
     {
-        throw new System.NotImplementedException();
+        CanAttack = true;
     }
 }
