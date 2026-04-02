cd51952176d6a9867630b5696a2fa7064cc5709b
jmmyzhang <155680424+jmmyzhang@users.noreply.github.com>
2026-04-01 22:02:26 -0400

    fix deathbringer spell offset, should add separate melee/ranged atk cd's

--- YamazGame/Assets/NewEnemy/Deathbringer/DeathbringerSpell/DeathbringerSpellSpawner.cs
+++ YamazGame/Assets/NewEnemy/Deathbringer/DeathbringerSpell/DeathbringerSpellSpawner.cs
@@ -4,10 +4,14 @@ public class DeathbringerSpellSpawner : MonoBehaviour
 {
     [SerializeField] GameObject spellPrefab;
     [SerializeField] EnemyDetectionRange detectionRange;
+    [SerializeField] Vector2 shadowOffset;
 
     public GameObject SpawnSpell()
     {
         if (!detectionRange.PlayerInRange) return null;
-        return Instantiate(spellPrefab, detectionRange.PlayerPosition, Quaternion.identity);
+
+        Vector3 spawnPos = detectionRange.PlayerPosition - shadowOffset;
+
+        return Instantiate(spellPrefab, spawnPos, Quaternion.identity);
     }
 }
