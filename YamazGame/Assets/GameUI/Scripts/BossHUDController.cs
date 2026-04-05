d62a9034e3482a42e31c25447c38ad49c14d1570
Fabian Herrera <fabianherrerap73@gmail.com>
2026-04-05 12:58:22 -0400

    Connect bosshealthbar

--- YamazGame/Assets/GameUI/Scripts/BossHUDController.cs
+++ YamazGame/Assets/GameUI/Scripts/BossHUDController.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
 
-
 public class BossHUDController : MonoBehaviour
-{} 
+{
+    [Header("Boss & Views")]
+    [SerializeField] private BossHealth bossHealth;
+    [SerializeField] private HealthBarView healthBarView;
+    [SerializeField] private HealthTextView healthTextView;
+
+    void Update()
+    {
+        if (bossHealth == null)
+        {
+            bossHealth = FindFirstObjectByType<BossHealth>();
+            if (bossHealth == null) return;
+        }
+
+        healthBarView.SetHealth(bossHealth.CurrentHealth, bossHealth.MaxHealth);
+        healthTextView.SetHealth(bossHealth.CurrentHealth, bossHealth.MaxHealth);
+    }
+}
 
