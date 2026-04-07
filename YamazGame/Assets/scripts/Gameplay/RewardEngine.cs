2352a787238eaea338d2590acc14f56cdbb0fad9
Ayman <aymanhayari@gmail.com>
2026-04-06 23:17:14 -0400

    Update RewardEngine.cs

--- YamazGame/Assets/scripts/Gameplay/RewardEngine.cs
+++ YamazGame/Assets/scripts/Gameplay/RewardEngine.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class RewardEngine : MonoBehaviour
 {
-    [SerializeField] private float difficultyMod = 1.008f;
+    [SerializeField] private float difficultyMod = 1.006f;
     [SerializeField] private int floorBonusBase = 1;
 
     public int Calculate(int floor, int baseKills)
     {
-        int floorBonus = floor * floorBonusBase;
+        int floorBonus = floor * floorBonusBase * 0.7;
         int total = Mathf.RoundToInt((baseKills * difficultyMod) + floorBonus);
         return total;
     }
